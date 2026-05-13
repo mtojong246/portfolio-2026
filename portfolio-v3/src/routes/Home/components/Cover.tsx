5580110df8bfa0267dc2617a2aca98af40da7bbc
Melanie Tojong <mtojong@Melanies-MBP.lan>
2026-05-13 08:43:13 -0400

    adding link icon buttons to cover

--- portfolio-v3/src/routes/Home/components/Cover.tsx
+++ portfolio-v3/src/routes/Home/components/Cover.tsx
@@ -1,8 +1,51 @@
+import { ReactComponent as LinkedinIcon } from '../../../assets/icons/Linkedin.svg';
+import { ReactComponent as GithubIcon } from '../../../assets/icons/Github.svg';
+import { ReactComponent as EmailIcon } from '../../../assets/icons/Email.svg';
+import { ReactNode } from 'react';
+
+const LinkButton = ({ icon, link }: { icon: ReactNode, link: string }) => {
+    return (
+        <a 
+            href={link}
+            target='_blank'
+            rel='noreferrer'
+            className='text-white hover:text-accent transition-all flex justify-center items-center aspect-square border border-outline w-[60px] rounded-full'
+        >
+            <div className='w-[24px]'>{icon}</div>
+        </a>
+    )
+}
+
+const links: { icon: ReactNode, link: string }[] = [
+    {
+        icon: <LinkedinIcon />,
+        link: 'https://www.linkedin.com/in/melanie-tojong-4b5149158/',
+    },
+    {
+        icon: <GithubIcon />,
+        link: 'https://github.com/mtojong246',
+    },
+    {
+        icon: <EmailIcon />,
+        link: 'mailto:mtojong@gmail.com',
+    }
+]
+
 export default function Cover() {
     return (
         <div className="w-full h-screen bg-dark">
-            <div className="flex flex-col justify-center items-center mx-auto">
-                
+            <div className="flex flex-col justify-center items-center mx-auto h-full max-w-[1200px]">
+                <h1 className="text-white text-[96px] font-bold mb-3">MELANIE TOJONG</h1>
+                <h3 className="text-white text-[22px] font-medium mb-[64px]">SOFTWARE ENGINEER, FRONTEND DEVELOPER & DESIGNER</h3>
+                <div className='flex justify-center items-center gap-6'>
+                    {links.map(iconLink => {
+                        const { icon, link } = iconLink;
+
+                        return (
+                            <LinkButton icon={icon} link={link}/>
+                        )
+                    })}
+                </div>
             </div>
         </div>
     )
