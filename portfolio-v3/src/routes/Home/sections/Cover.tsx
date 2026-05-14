dfbfe35be8fcead19093a89f507c3eed7254eb20
Melanie Tojong <mtojong@Melanies-MBP.lan>
2026-05-14 11:09:29 -0400

    adding transition animation to cover page components

--- portfolio-v3/src/routes/Home/sections/Cover.tsx
+++ portfolio-v3/src/routes/Home/sections/Cover.tsx
@@ -2,6 +2,7 @@ import { ReactComponent as LinkedinIcon } from '../../../assets/icons/Linkedin.s
 import { ReactComponent as GithubIcon } from '../../../assets/icons/Github.svg';
 import { ReactComponent as EmailIcon } from '../../../assets/icons/Email.svg';
 import { ReactNode } from 'react';
+import { motion, AnimatePresence } from 'motion/react';
 
 const LinkButton = ({ icon, link }: { icon: ReactNode, link: string }) => {
     return (
@@ -33,6 +34,7 @@ const links: { icon: ReactNode, link: string }[] = [
 
 
 export default function Cover() {
+
     return (
         <div className="w-full h-screen px-[64px] bg-dark relative">
             <div className='cover absolute top-0 left-0 right-0 bottom-0'>
@@ -47,14 +49,23 @@ export default function Cover() {
                 <div className='light x9'></div>
             </div>
             <div className='cover-fade absolute top-0 left-0 right-0 bottom-0 z-10'></div>
+            <AnimatePresence>
             <div className="flex flex-col justify-center items-center mx-auto h-full max-w-[1200px] relative z-20">
                 <h1 className="text-white text-[96px] font-bold mb-3">MELANIE TOJONG</h1>
-                <h3 
-                    className="cover-subtitle text-white text-[22px] font-medium mb-[64px]"
+                <motion.h3 
+                    initial={{ opacity: 0, y: 8 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.3, delay: 0.5, ease: 'easeIn' }}
+                    id='cover-subtitle'
+                    className="text-white text-[22px] font-medium mb-[64px]"
                 >
                     SOFTWARE ENGINEER, FRONTEND DEVELOPER & DESIGNER
-                </h3>
-                <div className='flex justify-center items-center gap-6'>
+                </motion.h3>
+                <motion.div 
+                    initial={{ opacity: 0, y: 8 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.3, delay: 0.8, ease: 'easeIn' }}
+                    className='flex justify-center items-center gap-6'>
                     {links.map(iconLink => {
                         const { icon, link } = iconLink;
 
@@ -62,13 +73,18 @@ export default function Cover() {
                             <LinkButton icon={icon} link={link}/>
                         )
                     })}
-                </div>
-                <div className="scroll-down">
+                </motion.div>
+                <motion.div 
+                    initial={{ opacity: 0}}
+                    animate={{ opacity: 1}}
+                    transition={{ duration: 0.3, delay: 1.1, ease: 'easeIn' }}
+                className="scroll-down">
                     <span></span>
                     <span></span>
                     <span></span>
-                </div>
+                </motion.div>
             </div>
+            </AnimatePresence>
             
         </div>
     )
