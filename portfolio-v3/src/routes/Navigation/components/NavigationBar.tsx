d2ded2838104a52db50dacc64902363f2e8260c0
Melanie Tojong <mtojong@Melanies-MBP.attlocal.net>
2026-05-20 21:21:04 -0400

    removing unused script

--- portfolio-v3/src/routes/Navigation/components/NavigationBar.tsx
+++ portfolio-v3/src/routes/Navigation/components/NavigationBar.tsx
@@ -9,18 +9,6 @@ const scrollToSection = (id: string) => {
     if (section) section.scrollIntoView({ behavior: 'smooth' })
 }
 
-{/* <title></title>
-<meta name="description" content="" />
-<meta property="og:title" content="" />
-<meta property="og:description" content="" />
-<meta name="theme-color" content="#FFFFFF" />
-<meta name="application-name" content="" />
-<link rel="icon" type="image/x-icon" href="favicon.ico" />
-<link rel="icon" type="image/png" href="favicon-196x196.png" sizes="196x196" />
-<link rel="icon" type="image/png" href="favicon-128x128.png" sizes="128x128" />
-<link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96" />
-<link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
-<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" /> */}
 
 
 export default function NavigationBar({
